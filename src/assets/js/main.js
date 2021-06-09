64a06cff9c2b70918cc15367185823ba3e1d9ee8
Andresh-maker <fanter8511@mail.ru>
2021-06-09 22:20:12 +0500

    step-1

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,10 +1,19 @@
 const input = document.querySelectorAll('input')
+const toggler = document.querySelectorAll('.questions__toggle')
 
 if (input) {
     input.forEach(item => {
         item.addEventListener('blur', function () {
             let label = this.parentElement.querySelector('label')
             this.value.length > 0 ? label.classList.add('active') : label.classList.remove('active')
+        })
+    })
+}
+
+if (toggler){
+    toggler.forEach(item => {
+        item.addEventListener('click', function (){
+            this.parentElement.querySelector('.questions__content').classList.toggle('active')
         })
     })
 }
