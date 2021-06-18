d75099cbc488e7359a60862418f4297114130e83
Andresh-maker <fanter8511@mail.ru>
2021-06-18 06:09:27 +0500

    fix: margin and padding for items

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -10,3 +10,10 @@ $('.questions__toggle').on('click', function () {
 $('.burger').on('click', function (){
     $(this).toggleClass('active')
 })
+$('.dropdown-list__item').on('click', function (e){
+    e.stopPropagation()
+    let inp = $(this).closest('.forms-language__group').find('input')
+    let label = $(this).closest('.forms-language__group').find('label')
+    label.addClass('active')
+    inp.val($(this).text().trim())
+})
