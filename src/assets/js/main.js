d7dcbbff3d309cedf7231f711f2c450476086af4
Andresh-maker <fanter8511@mail.ru>
2021-07-02 19:15:17 +0500

    fix: label for input

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -10,10 +10,18 @@ $('.questions__toggle').on('click', function () {
 $('.burger').on('click', function (){
     $(this).toggleClass('active')
 })
+
 $('.dropdown-list__item').on('click', function (e){
     e.stopPropagation()
     let inp = $(this).closest('.forms-language__group').find('input')
     let label = $(this).closest('.forms-language__group').find('label')
     label.addClass('active')
     inp.val($(this).find('span').first().text().trim())
+})
+
+$(function (){
+    $('input').each(function (){
+        let label = $(this).parent().find('label')
+        $(this).val().length > 0 ? label.addClass('active') : label.removeClass('active')
+    })
 })
